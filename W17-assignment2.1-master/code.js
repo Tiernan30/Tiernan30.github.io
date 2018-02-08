e7a5cdc5eb62cdd14df4ff8149af22059c681a8c
Jared Madden <tier.cerelli@hotmail.com>
2018-02-08 12:48:13 -0700

    update

--- W17-assignment2.1-master/code.js
+++ W17-assignment2.1-master/code.js
@@ -4,7 +4,7 @@
 function getStats(txt) {
     return {
         nChars: txt.length,
-        nWords: 21,
+        nWords: txt.split(' ', '-').length,
         nLines: 10,
         nNonEmptyLines: 22,
         averageWordLength: 3.3,
