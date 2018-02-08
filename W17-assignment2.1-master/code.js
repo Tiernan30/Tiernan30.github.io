6ec3999326b40608ed093b77b470fdbabbb069a0
Jared Madden <tier.cerelli@hotmail.com>
2018-02-08 13:13:37 -0700

    function push

--- W17-assignment2.1-master/code.js
+++ W17-assignment2.1-master/code.js
@@ -1,10 +1,18 @@
 //
 // this is just a stub for a function you need to implement
 //
+function getChars(txt){
+    return txt.length;
+}
+
+function getWords(txt){
+    var word = txt.match(/[\w\d]+/gi);
+    return word ? word.length : 0;
+}
 function getStats(txt) {
     return {
-        nChars: txt.length,
-        nWords: txt.trim().split(/\s+/).length,
+        nChars: getChars(txt),
+        nWords: getWords(txt),
         nLines: txt.split(/\r\n|\r|\n/).length,
         nNonEmptyLines: (txt.match(/^\s*\S/gm) || "").length,
         averageWordLength: 3.3,
